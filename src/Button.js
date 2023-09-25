e78384b8078d7fe1f137589c396bdf00584724b6
Asim Shrestha <asimmessi1@gmail.com>
2023-09-25 14:13:51 +1000

    commit

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Button = ({ cls, label, handleOnChange }) => {
+  return (
+    <div className={cls} onClick={() => handleOnChange(label)}>
+      {label}
+    </div>
+  );
+};
